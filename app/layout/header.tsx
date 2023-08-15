0db00bbf9e57001df742a867a09c22996fd644f0
Ubaid <ubaidurrehman14827@gmail.com>
2023-08-15 15:51:44 +0500

    Added Project

---
+++ app/layout/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-main w-full h-[80px] text-white'>
+        <div>hello</div>
+    </div>
+  )
+}
+
+export default Header
