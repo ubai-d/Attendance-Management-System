27236ee6d722900f6ad14794260059361b6875dc
Ubaid <ubaidurrehman14827@gmail.com>
2023-08-16 17:24:02 +0500

    signup ui completed

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -1,140 +1,115 @@
-// import React from "react";
-// function Signin() {
-//   return (
-//     <div className="flex bg-main h-screen items-center justify-center">
-//       <div className="flex flex-col bg-white w-[60%] h-[80%] rounded-xl">
-//         <div className="text-center my-10">
-//           <h1 className="text-3xl font-semibold text-sub">
-//             Welcome To U Attendance System
-//           </h1>
-//           <p className="mt-2">Register Yourself And Join A Great Community</p>
-//         </div>
-//         <div className="flex flex-row mx-20 justify-between">
-//           <div>
-//             <input
-//               placeholder="Enter Your First Name"
-//               className="bg-gray-200 px-16 py-3 rounded-full w-72 placeholder:text-black placeholder:text-start text-start"
-//             />
-//           </div>
-//           <div>
-//             <input
-//               placeholder="Enter Your First Name"
-//               className="bg-gray-200 px-16 py-3 rounded-full w-72 placeholder:text-black placeholder:text-start text-start"
-//             />
-//           </div>
-//         </div>
-//         <div>
-//           <div className="mx-20 my-5">
-//             <input
-//               placeholder="Enter Your Email Address"
-//               className="bg-gray-200 px-16 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start"
-//             />
-//           </div>
-//           <div className="mx-20 my-5">
-//             <input
-//               placeholder="Enter Your Password"
-//               className="bg-gray-200 px-16 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start"
-//             />
-//           </div>
-//           <div className="mx-20 my-5">
-//             <input
-//               placeholder="Confirm Yoour Password"
-//               className="bg-gray-200 px-16 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start"
-//             />
-//           </div>
-//           <div className="flex justify-between mx-40">
-//             <div className="flex gap-3">
-//             <input type="radio" />
-//             <p>teacher</p>
-//             </div>
-//            <div className="flex gap-3">
-//            <input type="radio" />
-//            <p>Student</p>
-//            </div>
-
-//           </div>
-//         </div>
-//         <div className="mx-auto">
-//           <button className="bg-sub px-10 py-3 rounded-xl">Register</button>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Signin;
-
+"use client"
 import React from "react";
-
+import { useFormik } from "formik";
+import { signUpSchema } from "../schemas";
 function Signup() {
+    const initialValues = {
+      FirstName: "",
+      LastName: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    };
+    const { values, handleBlur, handleChange, handleSubmit, errors, touched } =
+    useFormik({
+      initialValues,
+      validationSchema: signUpSchema,
+      validateOnChange: true,
+      validateOnBlur: false,
+      //// By disabling validation onChange and onBlur formik will validate on submit.
+      onSubmit: (values, action) => {
+        console.log("🚀 ~ file: App.jsx ~ line 17 ~ App ~ values", values);
+        //// to get rid of all the values after submitting the form
+        action.resetForm();
+      },
+    });
+    console.log(errors);
   return (
     <>
-    <div className="flex justify-center items-center bg-main h-screen">
-      <form className="bg-white h-[70%] w-[50%] rounded-2xl">
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-          <label htmlFor="First Name" className="input-label">
-            Name*
-          </label>
-          <input
-            type="name"
-            autoComplete="off"
-            name="First Name"
-            id="First Name"
-            placeholder="First Name"
-          />
+      <div className="flex justify-center items-center bg-main h-screen">
+        <form className="bg-white w-[60%] h-[80%] rounded-2xl">
+          <div className="text-center my-10">
+            <h1 className="text-3xl font-semibold text-sub">
+              Welcome To U Attendance System
+            </h1>
+            <p className="mt-2">Register Yourself And Join A Great Community</p>
           </div>
-          <div className="flex flex-col">
-          <label htmlFor="Last Name Name" className="input-label">
-            Name*
-          </label>
-          <input
-            type="name"
-            autoComplete="off"
-            name="Last Name Name"
-            id="Last Name Name"
-            placeholder="Last Name Name"
-          />
+          <div className="flex flex-row mx-16 gap-20 my-5">
+            <div className="flex flex-col">
+              <input
+                className="bg-gray-200 px-10 py-3 rounded-full w-72 placeholder:text-black placeholder:text-start text-start "
+                type="name"
+                autoComplete="off"
+                name="First Name"
+                id="First Name"
+                placeholder="First Name"
+                value={values.FirstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            <input
+              className="bg-gray-200 px-10 py-3 rounded-full w-72 placeholder:text-black placeholder:text-start text-start "
+              type="name"
+              autoComplete="off"
+              name="Last Name"
+              id="Last Name"
+              placeholder="Last Name"
+              value={values.LastName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
           </div>
-        </div>
-        <div className="input-block">
-          <label htmlFor="email" className="input-label">
-            Email*
-          </label>
-          <input
-            type="email"
-            autoComplete="off"
-            name="email"
-            id="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="input-block">
-          <label htmlFor="password" className="input-label">
-            Password*
-          </label>
-          <input
-            type="password"
-            autoComplete="off"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="input-block">
-          <label htmlFor="confirm_password" className="input-label">
-            Confirm Password*
-          </label>
-          <input
-            type="password"
-            autoComplete="off"
-            name="confirm_password"
-            id="confirm_password"
-            placeholder="Confirm Password"
-          />
-        </div>
-      </form>
+          <div className="mx-16 my-5">
+            <input
+              className="bg-gray-200 px-10 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start "
+              type="email"
+              autoComplete="off"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className="mx-16 my-5">
+            <input
+              className="bg-gray-200 px-10 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start "
+              type="password"
+              autoComplete="off"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className="mx-16 my-5">
+            <input
+              className="bg-gray-200 px-10 py-3 rounded-full w-[41rem] placeholder:text-black placeholder:text-start text-start "
+              type="password"
+              autoComplete="off"
+              name="confirm_password"
+              id="confirm_password"
+              placeholder="Confirm Password"
+              value={values.confirm_password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+             {errors.confirm_password && touched.confirm_password ? (
+                      <p className="form-error">{errors.confirm_password}</p>
+                    ) : null}
+          </div>
+          <div className="flex items-center justify-center">
+            {" "}
+            <button className="bg-sub px-10 py-3 rounded-xl" type="submit">
+              Register
+            </button>{" "}
+          </div>
+        </form>
       </div>
     </>
   );
