0223271657dbc673d1a31110887413951d9c2143
Ubaid <ubaidurrehman14827@gmail.com>
2023-08-22 22:28:34 +0500

    Changes made

--- app/Otp/page.tsx
+++ app/Otp/page.tsx
@@ -11,7 +11,7 @@ function Otp() {
     <div className="h-screen flex justify-center items-center space-x-2">
     {otp.map((_, index) => {
       return (
-        <div>
+        <div key={index}>
           <input
             type=""
             className="w-12 h-12 border-2 rounded bg-transparent outline-none text-center font-semibold text-xl spin-button-none border-gray-400 focus:border-gray-700 focus:text-gray-700 text-gray-400 transition spin-button-none"
